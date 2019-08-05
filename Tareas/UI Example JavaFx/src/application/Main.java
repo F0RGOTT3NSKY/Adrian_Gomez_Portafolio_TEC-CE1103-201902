f920ef073786ec2f23471843424295927b2456f3
Adrian Gomez <adriangomez2099@gmail.com>
2019-08-05 17:14:37 -0600

    Listo

--- Tareas/UI Example JavaFx/src/application/Main.java
+++ Tareas/UI Example JavaFx/src/application/Main.java
@@ -6,6 +6,8 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
 
+import javax.swing.JOptionPane;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
@@ -36,8 +38,16 @@ public class Main extends Application {
         
         BufferedReader br = null;
 		try {
-	         br =new BufferedReader(new FileReader("files/Libro1.csv"));
+	         br =new BufferedReader(new FileReader("files/Libro1_ColumnaExtra.csv"));
 	         String line = br.readLine();
+	         try {
+	        	 String [] Archivo = line.split(SEPARATOR);
+ 	        	 @SuppressWarnings("unused")
+				String ArchivoVacio = Archivo[0];
+ 	         }catch(Exception g){
+ 	        	System.err.println("ERROR el archivo esta vacio! "+g.getMessage());
+ 	        	JOptionPane.showMessageDialog(null, "El archivo esta vacio!", "ERROR", JOptionPane.ERROR_MESSAGE); 
+ 	         }
 	         while (null!=line){
 	 	         String [] fields = line.split(SEPARATOR);
 	 	         System.out.println(Arrays.toString(fields));
@@ -45,6 +55,19 @@ public class Main extends Application {
 	 	         fields = removeTrailingQuotes(fields);
 	 	         String Nombre = fields[0];
 	 	         String Segundo = fields[1];
+	 	         try {
+	 	        	 int ExcesoColumnas = fields.length;
+	 	        	 System.out.println(ExcesoColumnas);
+	 	        	 if (ExcesoColumnas==2) {
+	 	        		 System.out.println("");
+	 	        	 }
+	 	        	 else {
+	 	        		 System.out.println(fields[ExcesoColumnas]);	 	        
+	 	        	 }
+	 	         }catch(ArrayIndexOutOfBoundsException f) {
+	 	        	 System.err.println("ERROR el archivo tiene mas columnas de las permitidas asi que no se mostrarán los datos fuera de lo permitido!");
+	 	        	 JOptionPane.showMessageDialog(null, "El archivo tiene mas columnas de las permitidas asi que no se mostrarán los datos fuera de lo permitido!", "ERROR", JOptionPane.ERROR_MESSAGE); 
+	 	         }
 	 	         tableView.getItems().add(new Person(Nombre, Segundo));
 	 	         System.out.println(Arrays.toString(fields));
 	 	         line = br.readLine();   
@@ -52,6 +75,7 @@ public class Main extends Application {
 	         
 	      } catch (Exception e) {
 	         System.err.println("Error! "+e.getMessage());
+	         
 	      } finally {
 	         if (null!=br){
 	            try {
