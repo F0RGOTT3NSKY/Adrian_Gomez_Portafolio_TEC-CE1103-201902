ccb7b4325569be01d683433b3a08669cb1abc87d
Adrian Gomez <adriangomez2099@gmail.com>
2019-08-05 11:41:47 -0600

    Almost done1

--- Tareas/UI Example JavaFx/src/application/Main.java
+++ Tareas/UI Example JavaFx/src/application/Main.java
@@ -19,29 +19,38 @@ import javafx.stage.Stage;
 public class Main extends Application {
 	public static final String SEPARATOR=";";
 	public static final String QUOTE="\"";
-	public static int veces = 0;
-	
-	public static String CSVReader(int Num) {
-		
-		BufferedReader br = null;
+
+    public static void main(String[] args) {    	
+        launch(args);
+    }
+
+    @SuppressWarnings("unchecked")
+	@Override
+    public void start(Stage primaryStage) {
+        @SuppressWarnings("rawtypes")
+		TableView tableView = new TableView();
+        TableColumn<String, Person> column1 = new TableColumn<>("First Name");
+        column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
+        TableColumn<String, Person> column2 = new TableColumn<>("Last Name");
+        column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
+        tableView.getColumns().add(column1);
+        tableView.getColumns().add(column2);
+        
+        BufferedReader br = null;
 		String Change = null;
 		try {
 	         br =new BufferedReader(new FileReader("files/Libro1.csv"));
 	         String line = br.readLine();
-	         int temp = 0;
-	         while (null!=line && temp<=veces){
+	         while (null!=line){
 	 	         String [] fields = line.split(SEPARATOR);
-	 	         /**System.out.println(fields[1]);
-	 	         /**System.out.println(Arrays.toString(fields));*/
+	 	         System.out.println(Arrays.toString(fields));
 	 	            
 	 	         fields = removeTrailingQuotes(fields);
-	 	         /**System.out.println(Arrays.toString(fields));*/
-	 	         String res = fields[Num]; 
-	 	         String forward = res;
-	 	         Change = forward;
-	 	         veces++;
-	 	         line = br.readLine();
-	 	         temp++;    
+	 	         String Nombre = fields[0];
+	 	         String Segundo = fields[1];
+	 	         tableView.getItems().add(new Person(Nombre, Segundo));
+	 	         System.out.println(Arrays.toString(fields));
+	 	         line = br.readLine();   
 	         }
 	         
 	      } catch (Exception e) {
@@ -55,57 +64,16 @@ public class Main extends Application {
 	            }
 	         }
 	      }
-		System.out.println(Change);
-		return Change;
-		
-	}
-	private static String[] removeTrailingQuotes(String[] fields) {
+        VBox vbox = new VBox(tableView);
+        Scene scene = new Scene(vbox);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    private static String[] removeTrailingQuotes(String[] fields) {
 	      String result[] = new String[fields.length];
 	      for (int i=0;i<result.length;i++){
 	         result[i] = fields[i].replaceAll("^"+QUOTE,"").replaceAll(QUOTE+"$","");
 	      }
 	      return result;
 	   }
-    public static void main(String[] args) {
-    	
-        launch(args);
-    }
-
-    @SuppressWarnings("unchecked")
-	@Override
-    public void start(Stage primaryStage) {
-
-        @SuppressWarnings("rawtypes")
-		TableView tableView = new TableView();
-
-        TableColumn<String, Person> column1 = new TableColumn<>("First Name");
-        column1.setCellValueFactory(new PropertyValueFactory<>("firstName"));
-
-
-        TableColumn<String, Person> column2 = new TableColumn<>("Last Name");
-        column2.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-
-
-        tableView.getColumns().add(column1);
-        tableView.getColumns().add(column2);
-        
-        
-        String Nombre = CSVReader(0);
-        String Segundo = CSVReader(1);
-        
-        
-        
-        tableView.getItems().add(new Person(Nombre, Segundo));
-        String Nombre2 = CSVReader(0);
-        String Segundo2 = CSVReader(1);
-        tableView.getItems().add(new Person(Nombre2, Segundo2));
-
-        VBox vbox = new VBox(tableView);
-
-        Scene scene = new Scene(vbox);
-
-        primaryStage.setScene(scene);
-
-        primaryStage.show();
-    }
 }
